b38d73a267833ec58d301c76523b8cc451ba4cfd
guemplen <guemplen@gmail.com>
2024-06-02 10:58:55 +0200

    Fix ContactList module
    Providing filtering of contacts according to the value entered in the SearchBox

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,25 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { selectContacts, deleteContact } from '../../redux/contactsSlice';
+import { selectNameFilter } from '../../redux/filtersSlice';
 import styles from './ContactList.module.css';
 import ContactItem from '../Contact/Contact';
 
 const ContactList = () => {
   const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
   const dispatch = useDispatch();
 
   const handleDelete = id => {
     dispatch(deleteContact(id));
   };
 
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <ul className={styles.list}>
-      {contacts.map(contact => (
+      {filteredContacts.map(contact => (
         <ContactItem
           key={contact.id}
           contact={contact}
@@ -25,4 +31,3 @@ const ContactList = () => {
 };
 
 export default ContactList;
-
